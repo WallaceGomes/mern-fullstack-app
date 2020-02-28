0dc3c549d928666f33b2827b1615090f1903f605
unknown <wallacecardosogomes@gmail.com>
2020-02-27 21:23:27 -0300

    Adicionando rotas 'places'

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
-const bodyParser =  require('body-parser');
+//const bodyParser =  require('body-parser');
 
 const placesRoutes = require('./routes/places-routes');
 
 const app = express();
 
 //permite o uso das rotas configuradas no respectivo arquivo de rotas
-app.use(placesRoutes);
+//adicionando filtro no começo somente aceita caminhos começando com o filtro
+//pode haver mais variáveis após o filtro, mas não menos
+app.use('/api/places', placesRoutes); // /api/places/...
 
 app.listen(5000);
--- backend/routes/places-routes.js
+++ backend/routes/places-routes.js
@@ -2,11 +2,40 @@ const express = require('express');
 
 const router = express.Router();
 
+const TEST_PLACES = [
+    {
+        id: 'p1',
+        title: 'Teste Empire Building',
+        description: 'Description test',
+        location: {
+            lat: 40.7484474,
+            lng: -73.9871516
+        },
+        address: 'Teste adress',
+        creator: 'u1'
+    }
+];
+
+// A ordem das rotas tem importância na execução do código
+
 //recebe um endereço e uma função que será executada sempre que ele for chamado
-router.get('/', (req, res, next) => {
-    console.log('GET Request in Places');
+//adicionando ":" à rota indica um valor dinâmico
+router.get('/:pid', (req, res, next) => {
+    const placeid = req.params.pid;
+    const place = TEST_PLACES.find(p => {
+        return p.id === placeid;
+    });
     //qualquer dado enviado com esse método é convertido para json
-    res.json({message: 'It works!'});
+    res.json({place}); // => {place} => {place: place}
+});
+
+//recebe um id de usuário e retorna os "places" do mesmo
+router.get('/user/:uid', (req, res, next) => {
+    const userId = req.params.uid;
+    const place = TEST_PLACES.find(u => {
+        return u.creator === userId; 
+    });
+    res.json({place});
 });
 
 module.exports = router;
