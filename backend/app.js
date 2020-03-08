8e1f1c3ea0d2415215b84313480d938bf68e08e1
unknown <wallacecardosogomes@gmail.com>
2020-03-08 17:10:15 -0300

    Utilizando error models e lidando com erros nas rotas

--- backend/app.js
+++ backend/app.js
@@ -10,4 +10,21 @@ const app = express();
 //pode haver mais variáveis após o filtro, mas não menos
 app.use('/api/places', placesRoutes); // /api/places/...
 
+
+//midleware para quando houver um erro
+// o express indentifica automaticamente quando há um erro em qualquer outro midleware
+// e passa a utilizar esse com error
+//isso é possível por conta do quarto parâmetro, se há um quarto parâmetro,
+app.use((error, req, res, next) => {
+
+    //se uma resposta já foi enviada
+    if (res.headerSent) {
+        return next(error);
+    }
+
+    //mensagem de erro default
+    res.status(error.code || 500)
+    res.json({message: error.message || 'An unknown error occurred!'})
+})
+
 app.listen(5000);
--- backend/routes/places-routes.js
+++ backend/routes/places-routes.js
@@ -1,5 +1,7 @@
 const express = require('express');
 
+const HttpError = require('../models/http-error');
+
 const router = express.Router();
 
 const TEST_PLACES = [
@@ -25,7 +27,16 @@ router.get('/:pid', (req, res, next) => {
     const place = TEST_PLACES.find(p => {
         return p.id === placeid;
     });
+
+    //place => undefined
+    //standard error code 404
+    if (!place) {
+        throw new HttpError('Could not find a place for the ID!', 404); //http-error model
+        // aqui não precisa return pois já cancela a execução da função
+    }
+
     //qualquer dado enviado com esse método é convertido para json
+    //standard success code 200
     res.json({place}); // => {place} => {place: place}
 });
 
@@ -35,6 +46,15 @@ router.get('/user/:uid', (req, res, next) => {
     const place = TEST_PLACES.find(u => {
         return u.creator === userId; 
     });
+
+    //place => undefined
+    //standard error code 404
+    if (!place) {
+        return next(new HttpError('Could not find a place for the ID!', 404)); //http-error model
+    }
+
+    //qualquer dado enviado com esse método é convertido para json
+    //standard success code 200
     res.json({place});
 });
 
