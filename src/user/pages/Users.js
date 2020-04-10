14418e9cc173f830fefbec62aba9fb3ee37d93f4
unknown <wallacecardosogomes@gmail.com>
2020-04-10 18:54:17 -0300

    add: get users from backend > frontend

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,17 +1,58 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import UsersList from '../components/UsersList';
+import ErrorModal from '../../shared/components/UIElements/ErrorModal';
+import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
+
 
 const Users = () => {
-    const USERS = [
-        {
-        id: 'u1',
-        name: 'Wallace Gomes',
-        image: 'https://avatars3.githubusercontent.com/u/43701494?s=460&v=4',
-        places: 3
-    }
-    ];
 
-    return <UsersList items={USERS} />;
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState();
+    const [loadedUsers, setLoadedUsers] = useState();
+
+    //por padrão o fetch já é um get
+    //não precisa nenhum tipo de headernem body, pq não estou enviando nenhum dado
+    //NOTA: Não é uma boa prática utilizar async no useEffect
+    //caso utilize uma promisse, fazer usso de uma função para isso
+    useEffect(() => {
+        const sendRequest = async () => {
+            setIsLoading(true);
+            try{
+                const response = await fetch('http://localhost:5000/api/users');
+
+                //tem que usar o parser pois não vem json por padrão
+                const responseData = await response.json();
+
+                //uma resposta é ok se for do tipo 200
+                //respostas com mensagem 400 ou 500 não são ok logo serão tratados como erro aqui
+                if(!response.ok) {
+                    throw new Error(responseData.message);//vai pro catch
+                }
+
+                setLoadedUsers(responseData.users);
+            }catch(err) {
+                setError(err.message);
+            }
+            setIsLoading(false);
+        }
+        sendRequest();
+    }, []);
+
+    const errorHandler = () => {
+        setError(null);
+    }
+
+    return (
+        <React.Fragment>
+            <ErrorModal error={error} onClear={errorHandler} />
+            {isLoading && (
+                <div className="center" >
+                    <LoadingSpinner/>
+                </div>
+            )}
+            {!isLoading && loadedUsers && <UsersList items={loadedUsers} />}
+        </React.Fragment>
+    );
 };
 
 export default Users;
