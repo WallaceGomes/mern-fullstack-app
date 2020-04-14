1491d78ccb8e348b9a1e9b2800ad9f6c6ed44013
unknown <wallacecardosogomes@gmail.com>
2020-04-13 22:33:17 -0300

    chore: geting places byUserId

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -1,43 +1,67 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PlaceList from '../components/PlaceList';
 import { useParams } from 'react-router-dom';
 
-const TEMP_PLACES = [
-    {
-        id: 'p1',
-        title: 'Imagem Teste',
-        description: 'Descrição da imagem!',
-        imageUrl: 'https://media.gettyimages.com/photos/empire-state-building-at-sunset-picture-id171080501?s=612x612',
-        address: 'Endereço',
-        location: {
-            lat: 40.7484405,
-            lng: -73.9878584
-        },
-        creator: 'u1'
-    },
-    {
-        id: 'p2',
-        title: 'Imagem Teste 2',
-        description: 'Descrição da imagem 2!',
-        imageUrl: 'https://media.gettyimages.com/photos/empire-state-building-at-sunset-picture-id171080501?s=612x612',
-        address: 'Endereço',
-        location: {
-            lat: 40.7484405,
-            lng: -73.9878584
-        },
-        creator: 'u2'
-    }
-];
+// const TEMP_PLACES = [
+//     {
+//         id: 'p1',
+//         title: 'Imagem Teste',
+//         description: 'Descrição da imagem!',
+//         imageUrl: 'https://media.gettyimages.com/photos/empire-state-building-at-sunset-picture-id171080501?s=612x612',
+//         address: 'Endereço',
+//         location: {
+//             lat: 40.7484405,
+//             lng: -73.9878584
+//         },
+//         creator: 'u1'
+//     },
+//     {
+//         id: 'p2',
+//         title: 'Imagem Teste 2',
+//         description: 'Descrição da imagem 2!',
+//         imageUrl: 'https://media.gettyimages.com/photos/empire-state-building-at-sunset-picture-id171080501?s=612x612',
+//         address: 'Endereço',
+//         location: {
+//             lat: 40.7484405,
+//             lng: -73.9878584
+//         },
+//         creator: 'u2'
+//     }
+// ];
 
-const UserPlaces = () => {
+import { useHttpClient } from '../../shared/hooks/http-hook';
+import ErrorModal from '../../shared/components/UIElements/ErrorModal';
+import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
+
+const UserPlaces = async () => {
 
     //useParams = analisa a rota e pega oque é passado
     const userId = useParams().userId;
-    //filtra o conteúdo carregado e pega apenas os do userId
-    const loadedPlaces = TEMP_PLACES.filter(place => place.creator === userId);
+    const { isLoading, error, sendRequest, clearError} = useHttpClient();
+    const {loadedPlaces, setLoadedPLaces} = useState();
+
+    useEffect(() => {
+        const fetchPlaces = async () => {
+            try{
+                const responseData = await sendRequest(
+                    `http://localhost:5000/api/places/user/${userId}`
+                );
+                setLoadedPLaces(responseData.places);
+            }catch(err) {}
+        };
+        fetchPlaces();
+    }, [sendRequest, userId]);
 
     return (
-        <PlaceList items={loadedPlaces} />
+        <React.Fragment>
+            <ErrorModal error={error} onClear={clearError} />
+            {isLoading && (
+                <div className="center" >
+                    <LoadingSpinner/>
+                </div>
+            )}
+            {!isLoading && loadedPlaces && <PlaceList items={loadedPlaces} />}
+        </React.Fragment>
     );
 };
 
