23ddd50beafe7430e8ce735e0893abb7be54b110
unknown <wallacecardosogomes@gmail.com>
2020-04-27 23:12:27 -0300

    chore: Working with browser storage to auto login & logout

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Redirect, Switch} from 'react-router-dom';
 
 //Route = Define uma rota "path", é como um href
@@ -17,17 +17,35 @@ const App = () => {
 
   const [token, setToken] = useState(false);
   const [userId, setUserID] = useState(false);
+
   //necessário utilizar o useCallback para evitar a criação de novas funções e loops infinitos
   //sem dependências pois esta função não precisará ser recriada
   const login = useCallback((uid, token) => {
     setToken(token);
+    //gravando token no localstorage para continuar logado mesmo depois do reload
+    localStorage.setItem(
+      'userData',
+      JSON.stringify({
+        userId: uid,
+        token: token
+    }));
     setUserID(uid);
   },[]);
 
   const logout = useCallback(() => {
     setToken(null);
     setUserID(null);
+    //quando logout apaga as credenciais (userId, token)
+    localStorage.removeItem('userData');
   },[]);
+
+  //seguindo o ciclo de vida, o useEffect roda depois ao final do render
+  useEffect(() => {
+    const storedData = JSON.parse(localStorage.getItem('userData'));
+    if(storedData && storedData.token){
+      login(storedData.userId, storedData.token);
+    }
+  }, [login]);
 
   let routes;
 
